f5f19142c522815a65be5362cb3eda48e0212e0a
Jesus Gutierrez <99155468+jesWeb@users.noreply.github.com>
2023-03-11 22:40:15 -0600

    .

--- database/migrations/2023_03_10_040540_create_clientes_table.php
+++ database/migrations/2023_03_10_040540_create_clientes_table.php
@@ -14,7 +14,7 @@ class CreateClientesTable extends Migration
     public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
-            $table->id('id_clientes');
+            $table->id();
             $table->string('Nombre');
             $table->date('FechaN');
             $table->char('Sexo');
--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -7,12 +7,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="{{asset('js/jquery-3.3.1.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 
 </head>
 
 <header>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Drugslide</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">xa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </header>
 
 <body>
